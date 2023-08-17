384587279f8ed349f3c6216221ee991b7d81911a
Bogita Brian <bogitabrian@gmail.com>
2023-08-17 23:38:50 +0300

    Created a Redux store for my projects, added new files and connected Redux to the React app

---
+++ react-redux online_store project/src/Redux/actions/productActions.js
@@ -0,0 +1,16 @@
+//Action
+
+import { ActionTypes } from "../constants/action-types";
+export const setProducts = (products) => {
+    return{
+        type: ActionTypes.SET_PRODUCTS,
+        payload: products,
+    };
+};
+export const selectedProducts = (product) => {
+    return{
+        type: ActionTypes.SELECTED_PRODUCT,
+        payload: product,
+    };
+};
+
