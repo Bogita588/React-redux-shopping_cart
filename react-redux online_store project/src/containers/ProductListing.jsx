827cbca5d88d84ef9bfc56c3d63ccaa813e65905
Bogita Brian <bogitabrian@gmail.com>
2023-08-19 22:14:59 +0300

    Added new lines of code to the react components

--- react-redux online_store project/src/containers/ProductListing.jsx
+++ react-redux online_store project/src/containers/ProductListing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from "react-redux";
+import ProductComponent from './ProductComponent';
 
  const ProductListing = () => {
     const products = useSelector((state) => state);
